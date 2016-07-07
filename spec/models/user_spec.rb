00fa04fc99269cc8be112434a0a9a04d00732ce0
David Forero <davefogo@gmail.com>
2016-07-07 18:42:51 -0500

    add validations to wiki model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) {create(:user)}
 
+  it { is_expected.to have_many(:wikis)}
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Wiki, type: :model do
   let(:my_wiki) {create(:wiki)}
 
+  it {is_expected.to validate_presence_of(:title)}
+  it {is_expected.to validate_presence_of(:body)}
+  it {is_expected.to validate_presence_of(:user)}
+
+  it {is_expected.to validate_length_of(:title).is_at_least(5)}
+  it {is_expected.to validate_length_of(:body).is_at_least(20)}
+
   describe "attributes" do
     it "has title and body attributes" do
       expect(my_wiki).to have_attributes(title: my_wiki.title, body: my_wiki.body)
